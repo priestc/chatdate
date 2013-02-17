3fe95aeb5c826dfba93bfaaf4a41315916ad6d03
Chris Priest <cp368202@ohiou.edu>
2013-02-17 15:51:23 -0500

    removed foundation and added static serve folder

--- chatdate/chatdate/settings.py
+++ chatdate/chatdate/settings.py
@@ -45,11 +45,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticserve/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/staticserve/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
